286b0c34dbe5825a3ad6fe27ba3fd1c921d14bdf
tonyfiry <tbo79435@gmail.com>
2023-02-05 23:24:24 +0800

    python science

--- request.py
+++ request.py
@@ -29,9 +29,43 @@ import requests#匯入requests模組
 # print(r.text)#讀取JSON資料進來
 
 #取得HTTP回應內容(Respons centents)
-url_params = {'name':"陳萬安",'score':95}
-r = requests.get("http://httpbin.org/get",params=url_params)
-print(r.text)#讀取JSON資料進來
-print(r.content)#取得沒有編碼的位元組資料
-print(r.encoding)#取得html的編碼
-print(r.raw)#利用HTTPRespons物件
+# url_params = {'name':"陳萬安",'score':95}
+# r = requests.get("http://httpbin.org/get",params=url_params)
+# print(r.text)#讀取JSON資料進來
+# print(r.content)#取得沒有編碼的位元組資料
+# print(r.encoding)#取得html的編碼
+# print(r.raw)#利用HTTPRespons物件
+
+#檢查回應狀態碼
+# r = requests.get("https://www.youtube.com/")
+# print(r.status_code)#取得回應狀態碼是200
+# print(r.status_code == requests.codes.ok)#TRUE
+# r = requests.get("https://www.youtube.com/404")
+# print(r.status_code)#取得回應狀態碼是404
+# print(r.status_code == requests.codes.ok)#FALSE
+
+#取得回應狀態碼進一步資訊
+# r = requests.get("https://www.flag.com.tw/")
+# print(r.status_code)
+# print(r.raise_for_status())#取得進一步的資訊
+
+#取得標頭資訊
+# r = requests.get("https://www.flag.com.tw/")
+# print(r.headers['content-type'])#取得標頭資訊:content-type
+# print(r.headers['date'])#取得標頭資訊:date
+# print(r.headers['server'])#取得標頭資訊:server
+# print(r.headers.get('server'))#取得標頭資訊:server
+
+#存取Cookie的HTTP請求
+# url = "http://httpbin.org/cookies"
+# cookies = {'name':'tony','source':'89'}
+# r = requests.get(url,cookies=cookies)
+# print(r.text)
+
+#取得回應內容的Cookie資料
+seeion = requests.session()
+reponse = seeion.get("https://www.google.com/")
+v = seeion.cookies.get_dict()
+print(v)
+
+
